26544a9a0ac0bcbb1490213a6ee20184e4c74373
tobias <schindus89@gmx.de>
2024-05-17 20:47:04 +0200

    initial commit

---
+++ src/test/java/Easy/TwoSumTest.java
@@ -0,0 +1,47 @@
+package Easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TwoSumTest {
+    @Test
+    void testCaseExampleOne() {
+        // Given
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        // When
+        int[] result = TwoSum.twoSum(nums, target);
+
+        // Then
+        assertThat(result).isEqualTo(new int[]{0, 1});
+    }
+
+    @Test
+    void testCaseExampleTwo() {
+        // Given
+        int[] nums = {3,2,4};
+        int target = 6;
+
+        // When
+        int[] result = TwoSum.twoSum(nums, target);
+
+        // Then
+        assertThat(result).isEqualTo(new int[]{1, 2});
+    }
+
+    @Test
+    void testCaseExampleThree() {
+        // Given
+        int[] nums = {3,3};
+        int target = 6;
+
+        // When
+        int[] result = TwoSum.twoSum(nums, target);
+
+        // Then
+        assertThat(result).isEqualTo(new int[]{0, 1});
+    }
+
+}
