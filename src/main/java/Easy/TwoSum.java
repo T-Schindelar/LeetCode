b318293240dc4610c7286fc4efbc8982a5a194e3
tobias <schindus89@gmx.de>
2024-05-23 22:14:20 +0200

    Revert "update"
    This reverts commit c7bdca07471d43a5d3a3b3ba04cdcfef98b01a71.

---
+++ src/main/java/Easy/TwoSum.java
@@ -0,0 +1,43 @@
+package Easy;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.<br>
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.<br>
+ * You can return the answer in any order.<br>
+ * <br>
+ * Example 1:<br>
+ * Input: nums = [2,7,11,15], target = 9<br>
+ * Output: [0,1]<br>
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].<br>
+ * <br>
+ * Example 2:<br>
+ * Input: nums = [3,2,4], target = 6<br>
+ * Output: [1,2]<br>
+ * <br>
+ * Example 3:<br>
+ * Input: nums = [3,3], target = 6<br>
+ * Output: [0,1]<br>
+ * <br>
+ * Constraints:<br>
+ * 2 <= nums.length <= 10⁴<br>
+ * -10⁹ <= nums[i] <= 10⁹<br>
+ * -10⁹ <= target <= 10⁹<br>
+ * Only one valid answer exists.
+ */
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int sum;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                sum = nums[i] + nums[j];
+                if (sum == target) {
+                    return new int[] { i, j };
+                }
+
+            }
+        }
+        return null;
+    }
+}
