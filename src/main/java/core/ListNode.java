01ffb8abdaf7bf87240e509fece08ef17c3c7077
tobias <schindus89@gmx.de>
2024-07-12 22:32:36 +0200

    greenkeeping

--- src/main/java/core/ListNode.java
+++ src/main/java/core/ListNode.java
@@ -2,17 +2,21 @@ package core;
 
 public class ListNode {
 
+    @SuppressWarnings("checkstyle:VisibilityModifier")
     public int val;
 
+    @SuppressWarnings("checkstyle:VisibilityModifier")
     public ListNode next;
 
     public ListNode() {
     }
 
+    @SuppressWarnings("checkstyle:HiddenField")
     public ListNode(int val) {
         this.val = val;
     }
 
+    @SuppressWarnings("checkstyle:HiddenField")
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
